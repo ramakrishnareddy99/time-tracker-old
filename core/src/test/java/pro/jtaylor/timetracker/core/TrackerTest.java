11e01dc473b8dafbbfee0d84989ae4853fc68171
ramakrishnareddy99 <42637374+ramakrishnareddy99@users.noreply.github.com>
2018-09-24 15:29:21 +0530

    Update TrackerTest.java

--- core/src/test/java/pro/jtaylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/pro/jtaylor/timetracker/core/TrackerTest.java
@@ -2,12 +2,12 @@ package clinic.programming.timetracker.core;
 
 import static org.junit.Assert.*;
 
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import clinic.programming.timetracker.core.dao.TimeEntry;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import clinic.programming.timetracker.core.dao.TimeEntry;
 
 /**
  * Created by Jason on 6/19/2015.
